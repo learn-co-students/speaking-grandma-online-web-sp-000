eaa167ca2fc2963110f0cbc40026180a7a999383
Meisuleen Kased <meisuleen@gmail.com>
2018-10-31 17:57:39 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -12,17 +12,33 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-
- def speak_to_grandma(phrase)
-      if phrase == "I LOVE YOU GRANDMA!"
-        return "I LOVE YOU TOO PUMPKIN!"
-      elsif phrase == "Hi Nana, how are you?"
-        return "HUH?! SPEAK UP, SONNY!"
-      elsif phrase == "WHAT DID YOU EAT TODAY?"
-        return "NO, NOT SINCE 1938!"
-      elsif phrase == "WHAT?"
-        return "NO, NOT SINCE 1938!"
-      elsif phrase != phrase.upcase
-          return "HUH?! SPEAK UP, SONNY!"
-     end
+def speak_to_grandma(phrase)
+  if phrase != phrase.upcase
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase === "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase === phrase.upcase
+    return "NO, NOT SINCE 1938!"
   end
+end
+
+
+ # def speak_to_grandma(phrase)
+ #      if phrase == "I LOVE YOU GRANDMA!"
+ #        return "I LOVE YOU TOO PUMPKIN!"
+ #      elsif phrase == "Hi Nana, how are you?"
+ #        return "HUH?! SPEAK UP, SONNY!"
+ #      elsif phrase == "WHAT DID YOU EAT TODAY?"
+ #        return "NO, NOT SINCE 1938!"
+ #      elsif phrase == "WHAT?"
+ #        return "NO, NOT SINCE 1938!"
+ #      elsif phrase != phrase.upcase
+ #          return "HUH?! SPEAK UP, SONNY!"
+ #     end
+ #  end
+
+  # if phrase == phrase.downcase #look for if phrase is all lowercase
+  #   #code to run
+  # elsif phrase == phrase.upcase #look for if phrase is all uppercase
+  #   #code to run
+  # end
