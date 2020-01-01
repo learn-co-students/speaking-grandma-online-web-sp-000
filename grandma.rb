f3233f7eb50d8b446e63fb6cccf5597192f01ae5
Jonathan Koss <jonathan_koss@yahoo.com>
2020-01-01 02:35:05 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -15,13 +15,9 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == "Hi Nana, how are you?"
+  elsif phrase != phrase.upcase()
     "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == "Hi!"
-    "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == "WHAT DID YOU EAT TODAY?"
-    "NO, NOT SINCE 1938!"
-  elsif phrase == "WHAT?"
+  else 
     "NO, NOT SINCE 1938!"
   end 
 end 
