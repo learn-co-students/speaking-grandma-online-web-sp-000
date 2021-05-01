006c732b576216f502a14c28f12117a79a1bd197
John Kyeremeh <john.a.kyeremeh@gmail.com>
2021-05-01 17:20:19 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -3,15 +3,15 @@
 
 
 def speak_to_grandma(phrase)
-  if phrase == phrase.upcase 
-    return "NO, NOT SINCE 1938!"
-  elsif phrase == "I LOVE YOU GRANDMA!"
+  if phrase == "I LOVE YOU GRANDMA!"
    return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.upcase 
+    return "NO, NOT SINCE 1938!"
   else
     return "HUH?! SPEAK UP, SONNY!"
   end
- end
 end
+
 
 
 # Whatever you say to grandma, she should respond with
